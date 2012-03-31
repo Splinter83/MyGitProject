38b5a67241b261d7758e8598a0d2d167b019a8aa
Gabriele <fg0583@gmail.com>
2012-03-31 20:44:27 +0200

    my commit

---
+++ src/MyMain.java
@@ -0,0 +1,13 @@
+
+public class MyMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "why?";
+		System.out.println("My name is "+name);
+	}
+
+}
