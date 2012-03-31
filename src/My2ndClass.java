dceff3e6844cffbd3f5692ccdfab4d018b5bdcbd
Gabriele <fg0583@gmail.com>
2012-03-31 20:54:31 +0200

    my 3nd commit

---
+++ src/My2ndClass.java
@@ -0,0 +1,12 @@
+
+public class My2ndClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my 2nd Class");
+	}
+
+}
